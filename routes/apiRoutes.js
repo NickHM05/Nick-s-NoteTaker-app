a61af66609e7561f14f9262c80b04503c5028a26
NicholasHM05 <nhmamberger@gmail.com>
2023-01-18 21:59:28 -0600

    saving work with help from ta's

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,34 @@
-const router = require("express").Router(); //const store = require("../db/store");
+const router = require("express").Router();
+const store = require("../db/store");
 const db = require("../db/db.json");
 const path = require("path");
 const fs = require("fs");
 //gonna need an import store file called store.js under the db folder
 //gonna need a getAllnotes route.
 router.get('/notes', (req, res) => {
-    //     store
-    //     .getNotes()
-    //     .then((notes)=>{
-    //         return res.json(notes)
-    //     }).catch ((err)=> res.status(500).json(err))
-    return res.json(db)
+    store
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes)
+        }).catch((err) => res.status(500).json(err))
+    // return res.json(db)
 
 
 
 })
 router.post('/notes', (req, res) => {
-   const newNote = req.body
-   console.log(newNote);
-   let saveNote = db;
-   saveNote.push(newNote);
-   console.log(saveNote);
-   fs.writeFileSync(path.join(__dirname, "../db/db.json"),JSON.stringify(saveNote));
+    const newNote = req.body
+    console.log(newNote);
+    let saveNote = db;
+    saveNote.push(newNote);
+    console.log(saveNote);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(saveNote));
     return res.json(saveNote)
 
 
 
 })
-//gonna need a postNotes.route
+
 
 //gonna need a deleteNotes.route
 
